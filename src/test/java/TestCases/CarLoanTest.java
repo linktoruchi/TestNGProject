0b5c0715d29793edc3d1616a450eb03fc39cc263
Ruchi Sharma <ruchi.sharma01@nagarro.com>
2023-04-16 10:28:35 +0530

    Saving TestNG Project

---
+++ src/test/java/TestCases/CarLoanTest.java
@@ -0,0 +1,13 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class CarLoanTest {
+	
+	@Test
+	
+	public void test3() {
+		System.out.println("Inside car loan test 3");
+	}
+
+}
---
+++ src/test/java/TestCases/CreditCardTest.java
@@ -0,0 +1,14 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class CreditCardTest extends BaseClass {
+	
+	@Test
+	public void test4() {
+		
+		System.out.println("Inside credit card test");
+		
+	}
+
+}
